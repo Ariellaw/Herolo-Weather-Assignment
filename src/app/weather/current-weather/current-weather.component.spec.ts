5d8e2d65d28ac8ea30ca5c00c94db99f887eb57f
Ariella Wills <Ariellaw@gmail.com>
2020-03-28 20:37:50 +0300

    Fixed on of the automatic tests

--- src/app/weather/current-weather/current-weather.component.spec.ts
+++ src/app/weather/current-weather/current-weather.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CurrentWeatherComponent } from './current-weather.component';
+import { CurrentWeather } from 'src/app/models/current-weather.model';
+import { currentWeatherMockData } from 'src/app/mock-data/currentWeather';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
@@ -16,7 +18,7 @@ describe('CurrentWeatherComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrentWeatherComponent);
     component = fixture.componentInstance;
-    component.currentWeather = ;
+    component.currentWeather=CurrentWeather.fromJson(currentWeatherMockData[0])
     fixture.detectChanges();
   });
 
