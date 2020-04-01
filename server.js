7e8619108fda50b5edc3aa799017abea48dc178d
Ariella Wills <Ariellaw@gmail.com>
2020-04-01 22:23:59 +0300

    added cors

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+const cors = require("cors");
+
+app.use(
+    cors({
+      origin: ["https://dataservice.accuweather.com"],
+      credentials: true // enable set cookie
+    })
+  );
+  
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/herolo-weather-assignment'));
@@ -15,3 +24,4 @@ res.sendFile(path.join(__dirname+'/dist/herolo-weather-assignment/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+
