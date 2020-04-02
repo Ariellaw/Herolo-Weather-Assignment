8e555078442e2cb347f0feed6ab5bd42367994bc
Ariella Wills <Ariellaw@gmail.com>
2020-04-02 12:13:09 +0300

    adding constants

---
+++ src/app/models/constants.ts
@@ -0,0 +1,13 @@
+//  const apiKey:string = 'u3l9YgfcjX8dHIZH8x9mFVTNiGLuWh4y'
+const apiKey: string = 'KUf6QQD5JF8HgA7B11F4jna8eky8rZSA'
+const useMockData: boolean = true
+const units = {
+  fahrenheit: 'fahrenheit',
+  celsius: 'celsius'
+}
+const theme = {
+  darkmode: 'dark-mode',
+  lightmode: 'light-mode'
+}
+
+export { apiKey, useMockData, units, theme }
